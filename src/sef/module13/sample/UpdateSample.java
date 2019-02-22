f44cdf786d5544bdac5f5d8c9e0d132ac2914a2e
Aygul <aygul_ism@yahoo.com>
2019-02-22 18:23:27 +0200

    first commit

--- src/sef/module13/sample/UpdateSample.java
+++ src/sef/module13/sample/UpdateSample.java
@@ -18,7 +18,7 @@ public class UpdateSample {
             System.out.println("Connection successfully established!");
 
             //1 - Create a PreparedStatement with a query "update employee set salary=4900 where id=?"
-            PreparedStatement pStmt =cn.prepareStatement("update employee set salary=4900 where id=?") ;
+            PreparedStatement pStmt =cn.prepareStatement("update employee set salary=4900 where id=1") ;
 
             //    Change parameter to any keyword desired, if a record
             //     matches the query then it will be displayed
